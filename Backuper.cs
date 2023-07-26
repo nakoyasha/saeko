71af8b2207ca5f1da19c5de54227d3a94295a1eb
nakoyasha <96925398+nakoyasha@users.noreply.github.com>
2023-07-26 21:22:50 +0300

    Add project files.

---
+++ Backuper.cs
@@ -0,0 +1,66 @@
+﻿namespace Saeko
+{
+    public enum BackupStatus
+    {
+        Success,
+        Fail,
+    }
+
+    public static class DirectoryInfoExtensions
+    {
+        public static void DeepCopy(this DirectoryInfo directory, string destinationDir)
+        {
+            foreach (string dir in Directory.GetDirectories(directory.FullName, "*", SearchOption.AllDirectories))
+            {
+                string dirToCreate = dir.Replace(directory.FullName, destinationDir);
+                Directory.CreateDirectory(dirToCreate);
+            }
+
+            foreach (string newPath in Directory.GetFiles(directory.FullName, "*.*", SearchOption.AllDirectories))
+            {
+                Console.WriteLine($"[Backuper] {newPath} > {destinationDir}");
+                File.Copy(newPath, newPath.Replace(directory.FullName, destinationDir), true);
+            }
+
+        }
+    }
+
+    public class Backuper
+    {
+        private const string DefaultDirectory = "C:/SaekoBackup";
+        public string OutputDirectory = DefaultDirectory;
+
+        public IEnumerable<string> DirectoriesToBackup;
+
+        public Backuper(string OutputDirectory = DefaultDirectory, IEnumerable<string>? DirectoriesToBackup = null)
+        {
+            this.OutputDirectory = OutputDirectory;
+            this.DirectoriesToBackup = DirectoriesToBackup;
+        }
+
+        public BackupStatus Backup()
+        {
+
+            foreach (var DirectoryToBackup in this.DirectoriesToBackup)
+            {
+                Console.WriteLine($"[Backuper] Backuping up { DirectoryToBackup}"); 
+                var UserProfilePath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+
+                var OldPath = $"{UserProfilePath}\\{DirectoryToBackup}";
+                var NewPath = $"{OutputDirectory}\\{DirectoryToBackup}";
+
+
+                Directory.CreateDirectory(NewPath);
+                var sourceDir = new DirectoryInfo(OldPath);
+                sourceDir.DeepCopy(NewPath);
+            }
+
+            return BackupStatus.Success;
+        }
+
+        public void SetOutputDirectory(string OutputDirectory)
+        {
+            this.OutputDirectory = OutputDirectory;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using NativeFileDialogSharp;
+using Sharprompt;
+
+namespace Saeko
+{
+    public class Saeko {
+        public static void Main()
+        {
+            var DirectoriesToBackup = Prompt.MultiSelect("Directories to backup:", new[] {
+                "Downloads",
+                "Documents",
+                "Pictures",
+                "Videos",
+                "Music",
+                "Desktop",
+            }, pageSize: 3);
+
+            Console.WriteLine("Please select the output directory");
+            var OutputDirectory = Dialog.FolderPicker();
+
+            if (OutputDirectory.IsOk)
+            {
+                var BackupInstance = new Backuper(OutputDirectory.Path, DirectoriesToBackup);
+                Console.WriteLine($"Backup begin: {OutputDirectory.Path}");
+
+                BackupInstance.Backup();
+                Console.WriteLine($"Backup complete: {OutputDirectory.Path}");
+            } else if (OutputDirectory.IsError)
+            {
+                Console.WriteLine($"An error has occurred while opening the output directory: {OutputDirectory.ErrorMessage}");
+                Console.WriteLine("Aborting..");
+            }
+        }
+    }
+
+}
